9540ca65d72144ab62cce652c9ba5979a436a550
Marco Haubrich <marcolino.haubrich@gmail.com>
2024-06-05 21:19:09 -0300

    refactor: Update cache policy to force caching
    This commit updates the cache policy in the `setupApi` function of `get_it.dart` to `CachePolicy.forceCache`. This change ensures that the API responses are always cached, improving performance and reducing network requests.

--- lib/core/get_it.dart
+++ lib/core/get_it.dart
@@ -20,7 +20,7 @@ void setupApi({
           store: HiveCacheStore('${dir.path}/hiveCache'),
           hitCacheOnErrorExcept: [401, 403],
           maxStale: const Duration(days: 1),
-          policy: CachePolicy.noCache,
+          policy: CachePolicy.forceCache,
           priority: CachePriority.high,
         );
       },
