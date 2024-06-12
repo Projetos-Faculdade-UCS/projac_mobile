38b617864013c66f45cb4456f3b860581b855e02
Marco Haubrich <marhaubrich@ucs.br>
2024-06-12 14:51:32 -0300

    feat: :sparkles: Added icon

--- lib/app/home/widgets/home_drawer.dart
+++ lib/app/home/widgets/home_drawer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ionicons/ionicons.dart';
 import 'package:projac_mobile/app/_widgets/gradient_icon.dart';
+import 'package:projac_mobile/app/_widgets/svg_icon.dart';
 import 'package:projac_mobile/core/theme/main_theme.dart';
 import 'package:projac_mobile/routes.g.dart';
 import 'package:routefly/routefly.dart';
@@ -18,7 +19,19 @@ class HomeDrawer extends StatelessWidget {
             decoration: BoxDecoration(
               gradient: MainTheme.getGradient(Theme.of(context).brightness),
             ),
-            child: const Text('Drawer Header'),
+            child: const Row(
+              children: [
+                SvgIcon(size: 48, color: Colors.white),
+                SizedBox(width: 16),
+                Text(
+                  'Projac',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+              ],
+            ),
           ),
           ListTile(
             leading: const GradientIcon(Ionicons.folder_open_outline),
